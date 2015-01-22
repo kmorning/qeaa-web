bf04020e15688add76a9e7243140a42bdeed312a
Kelly Morning <kleemorning@gmail.com>
2015-01-21 21:58:18 -0500

    very basic group new, create, and show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,25 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    # Just to see what the parameters look like.
+    #render plain: params[:group].inspect
+    @group = Group.new(secure_params)
+
+    @group.save
+    redirect_to @group
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  private
+  def secure_params
+    params.require(:group).permit(:name, :address_building, :address_street,
+                                  :address_city, :weekday, :closed,
+                                  :accessible)
+  end
+
 end
