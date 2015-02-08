c2c95fcc03e10fd50199525fe02dd8361d5659c8
Kelly Morning <kleemorning@gmail.com>
2015-02-07 22:39:25 -0500

    fix typo in migration

--- db/migrate/20150208032429_remove_details_from_groups.rb
+++ db/migrate/20150208032429_remove_details_from_groups.rb
@@ -1,11 +1,11 @@
 class RemoveDetailsFromGroups < ActiveRecord::Migration
   def change
-    remove_column: :groups, :address_building, :string
-    remove_column: :groups, :address_street, :string
-    remove_column: :groups, :address_city, :string
-    remove_column: :groups, :weekday, :integer
-    remove_column: :groups, :time, :time
-    remove_column: :groups, :closed, :boolean
-    remove_column: :groups, :accessible, :boolean
+    remove_column :groups, :address_building, :string
+    remove_column :groups, :address_street, :string
+    remove_column :groups, :address_city, :string
+    remove_column :groups, :weekday, :integer
+    remove_column :groups, :time, :time
+    remove_column :groups, :closed, :boolean
+    remove_column :groups, :accessible, :boolean
   end
 end
