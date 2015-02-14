732ef609e4ae7966a75dcacfc442c0b828b6e7b0
Kelly Morning <kleemorning@gmail.com>
2015-02-13 20:04:48 -0500

    authorization without pundit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,19 @@
 class GroupsController < ApplicationController
+  before_filter :authenticate_account!
+  skip_before_filter :authenticate_account! , :only => [:index, :show]
   def new
+   # Temporay authorization without pundit
+    unless current_member.has_role? :admin
+      redirect_to :root, :alert => "Access denied."
+    end
     @group = Group.new
   end
 
   def create
+    # Temporay authorization without pundit
+    unless current_member.has_role? :admin
+      redirect_to :root, :alert => "Access denied."
+    end
     # Just to see what the parameters look like.
     #render plain: params[:group].inspect
     @group = Group.new(secure_params)
