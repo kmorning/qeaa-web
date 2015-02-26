14b0a4888bd270306820fedaccd8fa74f6984d9b
Kelly Morning <kleemorning@gmail.com>
2015-02-26 11:29:12 -0500

    make instance polymorphic in event migration

--- db/migrate/20150226152455_create_events.rb
+++ db/migrate/20150226152455_create_events.rb
@@ -2,11 +2,10 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.belongs_to :group, index: true
-      t.belongs_to :instance, index: true
+      t.references :instance, polymorphic: true, index: true
 
       t.timestamps null: false
     end
     add_foreign_key :events, :groups
-    add_foreign_key :events, :instances
   end
 end
