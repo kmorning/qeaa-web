8ea06d58787481927cacf684751d561480c1c90d
Kelly Morning <kleemorning@gmail.com>
2015-03-01 17:45:48 -0500

    fixed medallion create routing.  fixed medallion group through event.

--- app/models/medallion.rb
+++ app/models/medallion.rb
@@ -1,6 +1,6 @@
 class Medallion < ActiveRecord::Base
   has_one :event, as: :instance
-  has_one :group, through: :events
+  has_one :group, through: :event
   belongs_to :meeting
   belongs_to :member
 
