cb98a1b61c51d64509ae63b1a0c820a065ac948d
Kelly Morning <kleemorning@gmail.com>
2015-03-01 23:08:52 -0500

    add dependent destroy for medallion.event

--- app/models/medallion.rb
+++ app/models/medallion.rb
@@ -1,5 +1,5 @@
 class Medallion < ActiveRecord::Base
-  has_one :event, as: :instance
+  has_one :event, as: :instance, dependent: :destroy
   has_one :group, through: :event
   belongs_to :meeting
   belongs_to :member
