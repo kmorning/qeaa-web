0e8253731c1de39daadc608ff8e8f8a9403721f6
Kelly Morning <kleemorning@gmail.com>
2015-03-06 23:40:34 -0500

    get proper date in time zone by parsing Time.zone.now as Date

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -3,7 +3,7 @@ class BirthdaysController < ApplicationController
     # TODO: maybe put this logic in a model and allow for variable days
     # TODO: add birthday postable role to members, so that we can control
     # whether or not a birthday is automatically shown.
-    start_date = Date.today.in_time_zone
+    start_date = Date.parse(Time.zone.now.to_s)
     end_date = start_date.advance(days: +45)
     @members = Member.find_birthdays_for(start_date, end_date).sort_by{ |m| [m.birthday.month, m.birthday.day] }
   end
