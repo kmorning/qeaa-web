718a3297b8570a6927286c70436b0af1c3af8dc2
Kelly Morning <kleemorning@gmail.com>
2015-03-19 22:13:57 -0400

    use site helper method

---
+++ app/helpers/sites_helper.rb
@@ -0,0 +1,25 @@
+
+module SitesHelper
+  def area83_site_links
+    {
+      "Area 83 Eastern Ontario" => "http://area83aa.org/",
+      "District 30 Quinte West" => "http://www.quintewestaa.org",
+      "District 36 Kingston & The Islands"  => "http://www.kingstonaa.org",
+      "Greater Toronto Area InterGroup" => "http://aatoronto.org/",
+      "Oshawa Lake Shore InterGroup" => "http://www.aaoshawa.org/",
+      "Ottawa Area Intergroup" => "http://www.ottawaaa.org/",
+      "District 86 Kawartha" => "http://www.peterboroughaa.org/",
+      "District 66 The Golden Triangle" => "http://www.lanarkleedsaa.org/",
+      "District 74 Pembroke" => "http://www.pembrokeaa.org/",
+      "District 78 Madawaska Valley" => "http://aamadawaskavalley.org/",
+      "District 42 St. Lawrence International" => "http://aa-stlawrenceny.org/"
+    }
+  end
+
+  def aa_site_links
+    {
+      "World Service Organization" => "http://www.aa.org/",
+      "AA Grapevine" => "http://www.aagrapevine.org/"
+    }
+  end
+end
