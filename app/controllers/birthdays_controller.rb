ff73a6ac96a44f3fa376b6680ac64e838a4069c5
Kelly Morning <kleemorning@gmail.com>
2016-01-23 11:52:40 -0500

    change birthday advance from 45 to 30 days temporarily

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -4,7 +4,7 @@ class BirthdaysController < ApplicationController
     # TODO: add birthday postable role to members, so that we can control
     # whether or not a birthday is automatically shown.
     start_date = Date.parse(Time.zone.now.to_s)
-    end_date = start_date.advance(days: +45)
+    end_date = start_date.advance(days: +30)
     @members = Member.find_birthdays_for(start_date, end_date).sort_by{ |m| [m.birthday.month, m.birthday.day] }
   end
 end
