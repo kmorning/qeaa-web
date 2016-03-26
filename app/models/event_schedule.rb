07d7313086975b33a5b3dff1756c361a6b98e808
Kelly Morning <kleemorning@gmail.com>
2016-03-26 18:46:50 -0400

    implement model

--- app/models/event_schedule.rb
+++ app/models/event_schedule.rb
@@ -1,2 +1,33 @@
 class EventSchedule < ActiveRecord::Base
+  include Repeatable
+
+  extend TimeSplitter::Accessors
+  split_accessor :from, :to, default: -> { Time.zone.now }
+
+  serialize :schedule, IceCube::Schedule
+
+  # TODO:  do we need to belong to a calendar...I don't think so.
+  #belongs_to :calendar
+
+  validates_presence_of :name
+  #validates_presence_of :calendar_id
+  validates_presence_of :from
+  validates_presence_of :to
+
+  def color
+    if calendar.color
+      calendar.color
+    else
+      'lightblue'
+    end
+  end
+
+  def not_all_day?
+    if is_all_day
+      return false
+    else
+      return true
+    end
+  end
+
 end
