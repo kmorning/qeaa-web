17c254f6bf8763b213bc634220b1a6755208af87
Kelly Morning <kleemorning@gmail.com>
2016-03-28 14:33:12 -0400

    add class method to generate non-db events from schedule

--- app/models/corrections_event.rb
+++ app/models/corrections_event.rb
@@ -1,3 +1,59 @@
 class CorrectionsEvent < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
+  extend Rails.application.routes.url_helpers
+
   belongs_to :corrections_schedule, inverse_of: :corrections_events
+
+  attr_accessor :color :url :textColor
+
+  def self.occurrences_between(begin_date,end_date)
+    # Generate scheduled events that are not assigned and therefore not in db
+    # Using Squeel
+    # line 1 = Event doesn't repeat, but ends in window
+    # line 2 = Event doesn't repeat, but starts in window
+    # line 3 = Event doesn't repeat, but starts before and ends after
+    # line 4 = Event starts before our end date and repeats until a certain point of time, and that point of time after our begin date
+    # line 5 = Event repeats indefinitely, then all we care about is that it has started at somepoint in the last
+
+    results = CorrectionsSchedule.where{
+      (
+        (repeats == 'never') &
+        (from >= begin_date) &
+        (from <= end_date)
+      ) | (
+         (repeats == 'never') &
+         (to >= begin_date) &
+         (to <= end_date)
+      ) | (
+         (repeats == 'never') &
+         (from <= begin_date) &
+         (to >= end_date)
+      ) | (
+         (repeats != 'never') &
+         (from <= end_date) &
+         (repeat_ends == 'on') &
+         (repeat_ends_on >= begin_date)
+      ) | (
+         (repeats != 'never') &
+         (repeat_ends == 'never') &
+         (from <= end_date)
+      )
+    }
+
+    results.map { |event|
+      event.schedule.occurrences_between(begin_date,end_date).map { |date|
+        i = CorrectionsEvent.new()
+        i.title = event.name
+        i.color = event.calendar.color
+        i.url = Rails.application.routes.url_helpers.calendar_event_path(event)
+        i.start = date
+        i.end = date + event.duration.seconds
+        i.allDay = event.is_all_day
+        #i.event_id = event.id
+        i.textColor = 'black'
+        i
+      }
+    }.flatten.sort! {|x,y| x.start <=> y.start }
+  end
+
 end
