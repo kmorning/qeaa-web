1436e7ff445f90c18131c7f1c51368e335a45af6
kmorning <kleemorning@gmail.com>
2016-10-22 18:27:57 -0400

    Keep MarkerClusterer working after Google shut down svn-repos.  See gist https://gist.github.com/printercu/c4a02b8972ad76b6331b51b98c4b87a5

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,16 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+# google maps utility library
+Rails.application.config.assets.paths += %W(
+ #{Rails.root}/vendor/assets/google-maps-utility-library-v3
+)
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# google maps compile images
+Rails.application.config.assets.precompile += %w(
+ markerclustererplus/images/*.png
+)
