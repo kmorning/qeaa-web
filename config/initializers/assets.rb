8f6c37966c95f010f7f53e6800a3650356e2efc5
kmorning <kleemorning@gmail.com>
2016-10-22 23:57:47 -0400

    temporarily disable use of google maps utility library until it's confirmed that the git submodule gets deployed with capristano

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,21 +1,21 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+#Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # google maps utility library
-Rails.application.config.assets.paths += %W(
+#Rails.application.config.assets.paths += %W(
  #{Rails.root}/vendor/assets/google-maps-utility-library-v3
-)
+#)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
 # google maps compile images
-Rails.application.config.assets.precompile += %w(
- markerclustererplus/images/*.png
-)
+#Rails.application.config.assets.precompile += %w(
+# markerclustererplus/images/*.png
+#)
