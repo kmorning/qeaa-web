1f77eb5df96dea50713c1c3427649275543f3c55
Kelly Morning <kleemorning@gmail.com>
2021-04-30 18:50:25 -0400

    show past birthdays for 15 days

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -3,7 +3,9 @@ class BirthdaysController < ApplicationController
     # TODO: maybe put this logic in a model and allow for variable days
     # TODO: add birthday postable role to members, so that we can control
     # whether or not a birthday is automatically shown.
-    start_date = Date.parse(Time.zone.now.to_s)
+    #start_date = (Date.today - Date.today.mday + 1)
+    #start_date = Date.parse(Time.zone.now.to_s)
+    start_date = Date.today.advance(days: -15)
     end_date = start_date.advance(days: +45)
     @members = Member.find_birthdays_for(start_date, end_date).with_role(:viewable).sort_by{ |m| [m.birthday.month, m.birthday.day] }
   end
